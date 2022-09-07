c8299a56a28e8fd73d89a19a139b1abed043ac47
SimoAndreas1 <98739308+SimoAndreas1@users.noreply.github.com>
2022-09-07 09:02:45 +0300

    Add files via upload

---
+++ Harjutus1.java
@@ -0,0 +1,20 @@
+
+public class Harjutus1 {
+
+	public static void main(String[] args) {
+		// Nimi+kursus
+		System.out.println("Simo Andreas Novikov IT-21");
+		
+		// Täisarv ja komaarv
+		int vanus = 17;
+		double pikkus = 1.91;
+		String huudnimi = "Simo";
+		System.out.println(vanus + " " + pikkus + " " + huudnimi);
+		
+		int minuArv = 0;
+		System.out.println(minuArv);
+				
+
+	}
+
+}
