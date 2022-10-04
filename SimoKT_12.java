5a0c043347eae98f53661504c24ea995f404b678
SimoAndreas1 <98739308+SimoAndreas1@users.noreply.github.com>
2022-10-04 19:58:09 +0300

    Add files via upload

---
+++ SimoKT_12.java
@@ -0,0 +1,51 @@
+package simo_harjutused;
+
+public class SimoKT_12 {
+
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for (int i= 0; i<= 3-1 ; i++)
+	        {
+	            for (int j=0; j<=i; j++) { System.out.print("*"+ " "); } System.out.println(""); } for (int i=3-1; i>=0; i--)
+	        {
+	            for(int j=0; j <= i-1;j++)
+	            {
+	                System.out.print("*"+ " ");
+	            }
+	            System.out.println("");
+	        }
+	        for (int i= 5-1; i>=0 ; i--){
+	        	for (int j=0; j<=i; j++){
+	        		System.out.print("*");
+	        		}
+	        	System.out.println(" ");
+	            }
+	        
+	        System.out.println();
+	        
+	        for (int i = 1; i < 6; i++) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+				System.out.println("");
+			}	
+	        
+	        System.out.println();
+	        
+	        for (int i= 5-1; i>=0 ; i--){
+	        	for (int j=0; j<=i; j++){
+	        		System.out.print("*");
+	        		}
+	        	System.out.println(" ");
+	            }
+	        
+	        for (int i = 1; i < 6; i++) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+				System.out.println("");
+			}	
+	        
+		}
+
+	}
